ceca77295080689603ce1acd489cc5abf2a6d070
Rawniebrownie <57700226+Rawniebrownie@users.noreply.github.com>
2023-11-16 06:59:55 +0100

    Update readmy

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,4 @@
-import { createUmbracoClient } from "$lib/umbracoclient.js";
+import { createUmbracoClient } from '@thomasravnholt/umbracoclient';
 import { SECRET_API_KEY } from '$env/static/private';
 
 const umbracoApiDomain = 'http://attitydeheadless.designdev5.dk';
@@ -6,5 +6,13 @@ const umbracoApiKey = SECRET_API_KEY;
 
 const umbraco = createUmbracoClient(umbracoApiDomain, umbracoApiKey);
 
+async function fetchContent() {
+    try {
+        const content = await umbraco.getContentByType("person");
+        console.log(content);
+    } catch (error) {
+        console.error('Error fetching content:', error);
+    }
+}
 
-console.log(await umbraco.getContentByType("person"))
+console.log(fetchContent())
